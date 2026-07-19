074558f0a868cab02fb15ed91df849022c1dbd2a
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2026-07-19 22:22:19 +0200

    stop mlflow loading

--- src/ml/load_model.py
+++ src/ml/load_model.py
@@ -1,25 +1,14 @@
-import mlflow
 import os
 import pickle
 
-MODEL_PATH = os.getenv("MODEL_PATH", "models/model.pkl")
-MODEL_VERSION = os.getenv("MODEL_VERSION", "v1")
+MODEL_PATH = "models/model.pkl"
 
 def load_model():
-    """
-    Charge le modèle depuis un fichier .pkl ou depuis MLflow.
-    """
     if os.path.exists(MODEL_PATH):
         with open(MODEL_PATH, "rb") as f:
             model = pickle.load(f)
         print(f"✔ Modèle chargé depuis {MODEL_PATH}")
         return model
 
-    # Option MLflow (si activé plus tard)
-    try:
-        model = mlflow.pyfunc.load_model(f"models:/{MODEL_VERSION}")
-        print(f"✔ Modèle chargé depuis MLflow (version {MODEL_VERSION})")
-        return model
-    except Exception as e:
-        print("❌ Impossible de charger le modèle :", e)
-        raise
+    print("⚠ Aucun modèle trouvé. L'API démarre sans modèle.")
+    return None
